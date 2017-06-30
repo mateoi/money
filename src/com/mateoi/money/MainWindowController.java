a968df050cfd5648ab0a9dc970294a77935b1f1d
mateoi <mateo.inchaurrandieta@gmail.com>
2017-06-30 14:31:07 -0500

    Moved Transaction tab FXML to separate file

--- src/com/mateoi/money/MainWindowController.java
+++ src/com/mateoi/money/MainWindowController.java
@@ -1,4 +1,23 @@
 package com.mateoi.money;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+
 public class MainWindowController {
+
+    @FXML
+    Tab transactionTab;
+
+    @FXML
+    Tab accountsTab;
+
+    @FXML
+    Tab savingsTab;
+
+    @FXML
+    Tab budgetTab;
+
+    private void initialize() {
+
+    }
 }
